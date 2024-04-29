990dbdd31779ccae87dc898f42d3d2f6796c722c
alexec0de <serostanaleksej50@gmail.com>
2024-04-29 12:30:25 +0300

    add chat toggle

--- src/main/java/hw/zako/multichat/command/MultiChatToggleCommand.java
+++ src/main/java/hw/zako/multichat/command/MultiChatToggleCommand.java
@@ -1,2 +1,31 @@
-package hw.zako.multichat.command;public class MultiChatToggleCommand {
+// dev by alexec0de
+package hw.zako.multichat.command;
+
+import hw.zako.multichat.redis.RedisManager;
+import lombok.AllArgsConstructor;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+@AllArgsConstructor
+public class MultiChatToggleCommand implements CommandExecutor {
+
+    private final RedisManager redisManager;
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player)){
+            sender.sendMessage(ChatColor.RED + "Эту команду можно выполнить только в игре");
+            return true;
+        }
+        Player player = (Player) sender;
+        if (redisManager.getChatToggle().containsKey(player)){
+            redisManager.getChatToggle().remove(player);
+        } else {
+            redisManager.getChatToggle().put(player, true);
+        }
+
+        return true;
+    }
 }
--- src/main/java/hw/zako/multichat/redis/RedisManager.java
+++ src/main/java/hw/zako/multichat/redis/RedisManager.java
@@ -7,13 +7,19 @@ import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.async.RedisAsyncCommands;
 import io.lettuce.core.pubsub.StatefulRedisPubSubConnection;
+import lombok.Getter;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
 
 public class RedisManager {
     private static final String channel = "MULTICHAT_CHAT";
     private final RedisClient client;
     private final StatefulRedisConnection<String, String> connection;
     private final StatefulRedisPubSubConnection<String, String> pubSubConnection;
+    @Getter
+    public final HashMap<Player, Boolean> chatToggle = new HashMap<>();
 
     public RedisManager() {
         this.client = RedisClient.create("redis://"+ Config.redisConfig.host()+":"+Config.redisConfig.port());
@@ -33,7 +39,7 @@ public class RedisManager {
                         .replace("%sender%", event.getSender())
                         .replace("%message%", event.getMessage());
                 Bukkit.getOnlinePlayers().forEach(player -> {
-                    if (player.hasPermission(Config.chatUserPermission)) {
+                    if (player.hasPermission(Config.chatUserPermission) && !chatToggle.containsKey(player)) {
                         player.sendMessage(msg);
                     }
                 });
